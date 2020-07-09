0b661112272d7192ce4b0a157008ed35c215811c
TheIllusiveC4 <itwasmec4@gmail.com>
2020-07-08 19:18:31 -0700

    update curios integration

--- src/main/java/top/theillusivec4/corpsecomplex/common/modules/inventory/inventories/integration/CuriosInventory.java
+++ src/main/java/top/theillusivec4/corpsecomplex/common/modules/inventory/inventories/integration/CuriosInventory.java
@@ -26,12 +26,10 @@ import net.minecraft.nbt.ListNBT;
 import net.minecraftforge.common.util.Constants.NBT;
 import net.minecraftforge.items.ItemHandlerHelper;
 import top.theillusivec4.corpsecomplex.common.capability.DeathStorageCapability.IDeathStorage;
-import top.theillusivec4.corpsecomplex.common.modules.inventory.InventorySetting;
 import top.theillusivec4.corpsecomplex.common.modules.inventory.inventories.Inventory;
 import top.theillusivec4.corpsecomplex.common.util.Enums.InventorySection;
 import top.theillusivec4.corpsecomplex.common.util.InventoryHelper;
-import top.theillusivec4.curios.api.CuriosAPI;
-import top.theillusivec4.curios.api.capability.ICurioItemHandler;
+import top.theillusivec4.curios.api.CuriosApi;
 
 public class CuriosInventory implements Inventory {
 
@@ -39,17 +37,23 @@ public class CuriosInventory implements Inventory {
   public void storeInventory(IDeathStorage deathStorage) {
     PlayerEntity playerEntity = deathStorage.getPlayer();
     ListNBT list = new ListNBT();
-    CuriosAPI.getCuriosHandler(playerEntity)
-        .ifPresent(curioHandler -> curioHandler.getCurioMap().forEach((id, stackHandler) -> {
+    CuriosApi.getCuriosHelper().getCuriosHandler(playerEntity)
+        .ifPresent(curioHandler -> curioHandler.getCurios().forEach((id, stackHandler) -> {
           ListNBT list1 = new ListNBT();
+          ListNBT list2 = new ListNBT();
+
           for (int i = 0; i < stackHandler.getSlots(); i++) {
             InventoryHelper.process((PlayerEntity) curioHandler.getWearer(),
-                curioHandler.getStackInSlot(id, i), i, list1, InventorySection.CURIOS,
+                stackHandler.getStacks().getStackInSlot(i), i, list1, InventorySection.CURIOS,
                 deathStorage.getSettings().getInventorySettings());
+            InventoryHelper.process((PlayerEntity) curioHandler.getWearer(),
+                stackHandler.getCosmeticStacks().getStackInSlot(i), i, list2,
+                InventorySection.CURIOS, deathStorage.getSettings().getInventorySettings());
           }
           CompoundNBT tag = new CompoundNBT();
           tag.putString("Identifier", id);
           tag.put("Stacks", list1);
+          tag.put("CosmeticStacks", list2);
           list.add(tag);
         }));
     deathStorage.addInventory("curios", list);
@@ -62,29 +66,51 @@ public class CuriosInventory implements Inventory {
 
     if (player != null && oldPlayer != null) {
       ListNBT list = (ListNBT) oldStorage.getInventory("curios");
-      CuriosAPI.getCuriosHandler(player).ifPresent(newHandler -> {
+      CuriosApi.getCuriosHelper().getCuriosHandler(player).ifPresent(newHandler -> {
+
         for (int i = 0; i < list.size(); i++) {
           CompoundNBT tag = list.getCompound(i);
           String id = tag.getString("Identifier");
-          ListNBT stacks = tag.getList("Stacks", NBT.TAG_COMPOUND);
-          for (int j = 0; j < stacks.size(); j++) {
-            CompoundNBT compoundnbt = stacks.getCompound(j);
-            int slot = compoundnbt.getInt("Slot");
-            ItemStack itemstack = ItemStack.read(compoundnbt);
-            if (!itemstack.isEmpty()) {
-              ItemStack existing = newHandler.getStackInSlot(id, slot);
+          newHandler.getStacksHandler(id).ifPresent(stacksHandler -> {
+            ListNBT stacks = tag.getList("Stacks", NBT.TAG_COMPOUND);
 
-              if (existing.isEmpty()) {
-                newHandler.setStackInSlot(id, slot, itemstack);
-                CuriosAPI.getCurio(itemstack).ifPresent((curio) -> {
-                  player.getAttributes().applyAttributeModifiers(curio.getAttributeModifiers(id));
-                  curio.onEquipped(id, player);
-                });
-              } else {
-                ItemHandlerHelper.giveItemToPlayer(player, itemstack);
+            for (int j = 0; j < stacks.size(); j++) {
+              CompoundNBT compoundnbt = stacks.getCompound(j);
+              int slot = compoundnbt.getInt("Slot");
+              ItemStack itemstack = ItemStack.read(compoundnbt);
+
+              if (!itemstack.isEmpty()) {
+                ItemStack existing = stacksHandler.getStacks().getStackInSlot(slot);
+
+                if (existing.isEmpty()) {
+                  stacksHandler.getStacks().setStackInSlot(slot, itemstack);
+                  CuriosApi.getCuriosHelper().getCurio(itemstack).ifPresent((curio) -> {
+                    player.getAttributeManager().func_233793_b_(curio.getAttributeModifiers(id));
+                    curio.onEquip(id, slot, player);
+                  });
+                } else {
+                  ItemHandlerHelper.giveItemToPlayer(player, itemstack);
+                }
               }
             }
-          }
+            ListNBT cosmeticStacks = tag.getList("CosmeticStacks", NBT.TAG_COMPOUND);
+
+            for (int j = 0; j < cosmeticStacks.size(); j++) {
+              CompoundNBT compoundnbt = stacks.getCompound(j);
+              int slot = compoundnbt.getInt("Slot");
+              ItemStack itemstack = ItemStack.read(compoundnbt);
+
+              if (!itemstack.isEmpty()) {
+                ItemStack existing = stacksHandler.getCosmeticStacks().getStackInSlot(slot);
+
+                if (existing.isEmpty()) {
+                  stacksHandler.getCosmeticStacks().setStackInSlot(slot, itemstack);
+                } else {
+                  ItemHandlerHelper.giveItemToPlayer(player, itemstack);
+                }
+              }
+            }
+          });
         }
       });
     }
